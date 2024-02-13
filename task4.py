308f5a4ceff8514c13665d723ac9bf456e7af223
MihailInternul <157023284+MihailInternul@users.noreply.github.com>
2024-02-13 09:58:32 +0200

    task4.py

--- task4.py
+++ task4.py
@@ -1,3 +1,5 @@
+# Given an input string, count occurrences of all characters within a string (e.g. pythonnohtyppy -> p:3, y:3, t:2, h:2, o:2, n:2).
+
 def count_occurrences(input_string):
     char_count = {}
 
