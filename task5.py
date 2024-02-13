7f66ad8e9a1ac3ac2e202e2418abdcf5f5d4a8a8
MihailInternul <157023284+MihailInternul@users.noreply.github.com>
2024-02-13 10:00:27 +0200

    task5.py

--- task5.py
+++ task5.py
@@ -1,3 +1,8 @@
+# Write a script that gets system information like distro info, memory(total, used, free),
+# CPU info (model, core numbers, speed), current user, system load average, and IP address.
+# Use arguments for specifying resources. (For example, -d for distro -m for memory, 
+# -c for CPU, -u for user info, -l for load average, -i for IP address).
+
 import platform
 import psutil
 import socket
